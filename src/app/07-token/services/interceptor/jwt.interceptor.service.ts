23c4d30f07e090f28e66787ca47e6934cc0291cb
sergio-grr <sejagarrido@gmail.com>
2024-05-20 15:06:09 +0200

    comentarios codigo token

--- src/app/07-token/services/interceptor/jwt.interceptor.service.ts
+++ src/app/07-token/services/interceptor/jwt.interceptor.service.ts
@@ -7,8 +7,9 @@ import {
   providedIn: 'root'
 })
 export class JwtInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    let cloneRequest = req;
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {//req es eñ pbketp de la solicitud http y next envia la solicitud al servidor
+    let cloneRequest = req; // crea una copia para modificarla  para no modificar la original
     if(cloneRequest.url.includes("https://8rkrz.wiremockapi.cloud/verifyToken")){
       let token = localStorage.getItem("token")
       cloneRequest = req.clone(
@@ -21,6 +22,6 @@ export class JwtInterceptor implements HttpInterceptor {
       console.log(cloneRequest);
     }
     return next.handle(cloneRequest);
-    
+
   }
 }
--- src/app/07-token/services/interceptor/spinner.interceptor.service.ts
+++ src/app/07-token/services/interceptor/spinner.interceptor.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor, HttpResponse
-} from '@angular/common/http';
+  HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { LoadingService } from '../loading.service';
@@ -16,18 +12,18 @@ export class SpinnerInterceptor implements HttpInterceptor {
     private _loading: LoadingService
   ) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this._loading.setLoading(true, request.url);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { //reques coge la solicitud http y next la envia al servidor
+    this._loading.setLoading(true, request.url);// intercepta la llamada, y coloca el el spinner en true mientras dura el evento
     return next.handle(request).pipe(
       catchError((err) => {
         this._loading.setLoading(false, request.url);
-        throw err; // Necesitas lanzar el error nuevamente después de manejarlo
+        throw err;
       }),
       map((evt: HttpEvent<any>) => {
         if (evt instanceof HttpResponse) {
           this._loading.setLoading(false, request.url);
         }
-        return evt;
+        return evt; // devuelve el evento para que el flujo de datos continue
       })
     );
   }
