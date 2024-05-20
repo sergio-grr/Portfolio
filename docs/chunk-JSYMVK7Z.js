0aab5a08f2d5c42d30e9b435d9dc2ecaf5e80d58
sergio-grr <sejagarrido@gmail.com>
2024-05-20 17:07:59 +0200

    add responsive

--- docs/chunk-JSYMVK7Z.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-5YI7QLYQ.js";import"./chunk-DKHARI2P.js";import"./chunk-WXI33M2S.js";import"./chunk-3C6EQAVK.js";import"./chunk-XDLHON2M.js";export{a as AppModule};
--- docs/main-22LMA6WC.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./chunk-5YI7QLYQ.js";import"./chunk-DKHARI2P.js";import"./chunk-WXI33M2S.js";import"./chunk-3C6EQAVK.js";import{Eb as o}from"./chunk-XDLHON2M.js";o().bootstrapModule(r).catch(t=>console.error(t));
