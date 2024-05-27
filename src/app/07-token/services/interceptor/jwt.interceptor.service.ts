1ea4c7a69044394ff230fd57ec4737eee7cea31d
sergio-grr <sejagarrido@gmail.com>
2024-05-27 08:18:09 +0200

    commit error

--- src/app/07-token/services/interceptor/jwt.interceptor.service.ts
+++ src/app/07-token/services/interceptor/jwt.interceptor.service.ts
@@ -8,7 +8,7 @@ import {
 })
 export class JwtInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {//req es eñ pbketp de la solicitud http y next envia la solicitud al servidor
+  intercept(req: HttpRequest<any>, next: HttpHandler) {//req es el que envia de la solicitud http y next envia la solicitud al servidor
     let cloneRequest = req; // crea una copia para modificarla  para no modificar la original
     if(cloneRequest.url.includes("https://8rkrz.wiremockapi.cloud/verifyToken")){
       let token = localStorage.getItem("token")
