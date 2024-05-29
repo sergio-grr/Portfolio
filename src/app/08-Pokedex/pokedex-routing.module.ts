b0072981c0c564fc9ad71543f2b8a110cb2b9217
sergio-grr <sejagarrido@gmail.com>
2024-05-29 16:17:52 +0200

    v.mobile

--- src/app/08-Pokedex/pokedex-routing.module.ts
+++ src/app/08-Pokedex/pokedex-routing.module.ts
@@ -2,6 +2,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokeMainPageComponent } from './pages/poke-main-page/poke-main-page.component';
+import { Path } from 'leaflet';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 const routes: Routes =[
@@ -9,6 +11,9 @@ const routes: Routes =[
   {
     path: '',
     component: PokeMainPageComponent
+  }, {
+    path: 'home',
+    component: HomePageComponent
   },
 
 
--- src/app/08-Pokedex/pokedex.module.ts
+++ src/app/08-Pokedex/pokedex.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PokeMainPageComponent } from './pages/poke-main-page/poke-main-page.component';
 import { PokedexRoutingModule } from './pokedex-routing.module';
-import { BorderYellowComponent } from './components/border-yellow/border-yellow.component';
-import { BorderBlackComponent } from './components/border-black/border-black.component';
-import { ScreenComponent } from './components/screen/screen.component';
+
 import { ArrowsComponent } from './components/arrows/arrows.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
 
 
 @NgModule({
@@ -15,11 +12,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
     PokeMainPageComponent,
     HomePageComponent,
 
-    BorderYellowComponent,
-    BorderBlackComponent,
-    ScreenComponent,
+
     ArrowsComponent,
     ButtonsComponent,
+
   ],
   imports: [
     PokedexRoutingModule,
